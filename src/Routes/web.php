35e9b4e00184f6ec156b7a6e2f4d7ead83776509
olbap-web <pa.garrido.cid@gmail.com>
2025-07-10 17:04:42 -0400

    manejo de rutas

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -3,24 +3,25 @@
 use Slim\Routing\RouteCollectorProxy;
 use App\Controllers\UserController;
 use App\Controllers\ErrorController;
-
-
 use App\Middleware\FirebaseAuthMiddleware;
-
 
 $app->group('', function (RouteCollectorProxy $group) {
 
-    // $group->get('',[api])
-
-    $group->get('/', [ErrorController::class, 'notAuth']); // opcional
-    $group->get('**', [ErrorController::class, 'notAuth']); // opcional
-
-
-    // Ruta protegida con Firebase
+    // Rutas protegidas dentro de /api
     $group->group('/api', function (RouteCollectorProxy $auth) {
         $auth->get('/user', [UserController::class, 'getUserByEmail']);
         $auth->get('/reminder', [UserController::class, 'getReminderByUser']);
-
-        
     })->add(new FirebaseAuthMiddleware());
+
+
+    $group->map(['GET', 'POST', 'PUT', 'DELETE'], '/{routes:.+}', function ($request, $response) {
+        $uri = $request->getUri()->getPath();
+        if (strpos($uri, '/api') !== 0) {
+            return (new ErrorController)->notAuth($request, $response, []);
+        }
+
+        return $response->withStatus(404)->withHeader('Content-Type', 'application/json')
+                        ->write(json_encode(['error' => 'API route not found']));
+    });
+
 });
