60518514731c709817a0492103782df529f7d05b
olbap-web <pa.garrido.cid@gmail.com>
2025-07-10 18:53:17 -0400

    changes on userController

--- public/index.php
+++ public/index.php
@@ -8,17 +8,15 @@ $app = AppFactory::create();
 $app->addBodyParsingMiddleware();
 
 
-// ✅ Middleware global para CORS
 $app->add(function ($request, $handler) {
     $response = $handler->handle($request);
     return $response
-        ->withHeader('Access-Control-Allow-Origin', '*') // ← cámbialo por tu dominio en prod
+        ->withHeader('Access-Control-Allow-Origin', '*')
         ->withHeader('Access-Control-Allow-Headers', 'Authorization, Content-Type, Accept, Origin')
         ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
         ->withHeader('Access-Control-Allow-Credentials', 'true');
 });
 
-// ✅ Ruta de preflight (OPTIONS)
 $app->options('/{routes:.+}', function ($request, $response) {
     return $response;
 });
--- src/Controllers/UserController.php
+++ src/Controllers/UserController.php
@@ -39,13 +39,16 @@ class UserController
         $userFn = new UserService();
         $result = $userFn->getPersonaByEmail($email); 
 
-        if($result['status'] !=200){
-            $response->getBody()->write(json_encode([
-                "message"=>$result['message'],
-                "error"=>true,
-            ]));
-            return $response->withStatus(400)->withHeader('Content-Type', 'application/json');
+        if(isset($result['status'])){
+            if($result['status'] !=200){
+                $response->getBody()->write(json_encode([
+                    "message"=>$result['message'],
+                    "error"=>true,
+                ]));
+                return $response->withStatus(400)->withHeader('Content-Type', 'application/json');
+            }
         }
+        
 
         $response->getBody()->write(json_encode($result));
         return $response->withHeader('Content-Type', 'application/json');
