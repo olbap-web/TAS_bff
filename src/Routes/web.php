5c2eff0e39c33f87bc52185b8f954e584df9108b
olbap-web <pa.garrido.cid@gmail.com>
2025-07-11 01:10:26 -0400

    asdasd

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -13,14 +13,14 @@ use App\Middleware\FirebaseAuthMiddleware;
 
 $app->group('', function (RouteCollectorProxy $group) {
 
-    $group->get('/user', [UserController::class, 'getUserByEmail']);
+    // $group->get('/user', [UserController::class, 'getUserByEmail']);
     $group->get('/reminder/family-group', [ReminderController::class, 'getReminderByFammilyGroup']);
     $group->get('/family-group/persona', [FamilyGroupController::class, 'getFamilyGroupByPersona']);
     $group->get('/pet/family-group', [PetController::class, 'getPetsByFamilyGroup']);
 
     // Rutas protegidas dentro de /api
     $group->group('/api', function (RouteCollectorProxy $auth) {
-        // $auth->get('/user', [UserController::class, 'getUserByEmail']);
+        $auth->get('/user', [UserController::class, 'getUserByEmail']);
         // $auth->get('/reminder/family-group', [ReminderController::class, 'getReminderByFammilyGroup']);
         // $auth->get('/family-group/persona', [FamilyGroupController::class, 'getFamilyGroupByPersona']);
         // $auth->get('/pet/family-group', [PetController::class, 'getPetsByFamilyGroup']);
@@ -35,8 +35,7 @@ $app->group('', function (RouteCollectorProxy $group) {
             return (new ErrorController)->notAuth($request, $response, []);
         }
 
-        return $response->withStatus(404)->withHeader('Content-Type', 'application/json')
-                        ->write(json_encode(['error' => 'API route not found']));
+        return $response->withStatus(404)->withHeader('Content-Type', 'application/json')->write(json_encode(['error' => 'API route not found']));
     });
 
 });
