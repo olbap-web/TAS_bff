d2f8ace43990930cff8a2a5334c7033078ece3f2
olbap-web <pa.garrido.cid@gmail.com>
2025-07-12 15:07:07 -0400

    asdasd

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -59,13 +59,13 @@ $app->group('/api', function (RouteCollectorProxy $group) {
     })->add(new FirebaseAuthMiddleware());
 
 
-    $group->map(['GET', 'POST', 'PUT', 'DELETE'], '/{routes:.+}', function ($request, $response) {
-        $uri = $request->getUri()->getPath();
-        if (strpos($uri, '/api') !== 0 || $uri == '/') {
-            return (new ErrorController)->notAuth($request, $response, []);
-        }
+    // $group->map(['GET', 'POST', 'PUT', 'DELETE'], '/{routes:.+}', function ($request, $response) {
+    //     $uri = $request->getUri()->getPath();
+    //     if (strpos($uri, '/api') !== 0 || $uri == '/') {
+    //         return (new ErrorController)->notAuth($request, $response, []);
+    //     }
 
-        return $response->withStatus(404)->withHeader('Content-Type', 'application/json')->write(json_encode(['error' => 'API route not found']));
-    });
+    //     return $response->withStatus(404)->withHeader('Content-Type', 'application/json')->write(json_encode(['error' => 'API route not found']));
+    // });
 
 });
