ba67274fefd5f6063d4020c7a98d494261dd0ab9
olbap-web <pa.garrido.cid@gmail.com>
2025-07-12 20:18:30 -0400

    asdasd

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -18,39 +18,37 @@ use App\Middleware\FirebaseAuthMiddleware;
 $app->group('/api', function (RouteCollectorProxy $group) {
 
     // $group->get('/reminder', [ReminderController::class, 'getReminderByPk']);
-        $group->get('/user', [UserController::class, 'getUserByEmail']);
-        $group->get('/user/persona', [UserController::class, 'getUserByRut']);
-        
-        $group->get('/reminder/user', [ReminderController::class, 'getReminderByUser']);
-        $group->get('/reminder', [ReminderController::class, 'getReminderByPk']);
+    // $group->get('/user', [UserController::class, 'getUserByEmail']);
+    // $group->get('/user/persona', [UserController::class, 'getUserByRut']);
+    
+    // $group->get('/reminder/user', [ReminderController::class, 'getReminderByUser']);
+    // $group->get('/reminder', [ReminderController::class, 'getReminderByPk']);
 
 
-        $group->get('/family-group/persona', [FamilyGroupController::class, 'getFamilyGroupByPersona']);
+    // $group->get('/family-group/persona', [FamilyGroupController::class, 'getFamilyGroupByPersona']);
 
-        /**family-group/pets  -> hace referencia a las mascotas del grupo familiar ... cambiar esto */
+    // /**family-group/pets  -> hace referencia a las mascotas del grupo familiar ... cambiar esto */
 
-        $group->get('/pet/family-group', [PetController::class, 'getPetsByFamilyGroup']);
-        $group->get('/pet/detail', [PetController::class, 'getPetByPk']);
+    // $group->get('/pet/family-group', [PetController::class, 'getPetsByFamilyGroup']);
+    // $group->get('/pet/detail', [PetController::class, 'getPetByPk']);
 
-        $group->get('/medical-ctrl/pet', [MedicalCtrlController::class, 'getMedicalCtrlByPet']);
-        $group->get('/medical-ctrl', [MedicalCtrlController::class, 'getMedicalCtrlByPk']);
+    // $group->get('/medical-ctrl/pet', [MedicalCtrlController::class, 'getMedicalCtrlByPet']);
+    // $group->get('/medical-ctrl', [MedicalCtrlController::class, 'getMedicalCtrlByPk']);
 
-        $group->get('/treatment', [TreatmentController::class, 'getTreatmentByPk']);
-        $group->get('/treatment/documents', [TreatmentController::class, 'getDocumentsByTreatment']);
-        $group->get('/treatment/medicine', [TreatmentController::class, 'getMedicineByTreatment']);
-        $group->get('/treatment/pet', [TreatmentController::class, 'getTreatmentsByPet']);
+    // $group->get('/treatment', [TreatmentController::class, 'getTreatmentByPk']);
+    // $group->get('/treatment/documents', [TreatmentController::class, 'getDocumentsByTreatment']);
+    // $group->get('/treatment/medicine', [TreatmentController::class, 'getMedicineByTreatment']);
+    // $group->get('/treatment/pet', [TreatmentController::class, 'getTreatmentsByPet']);
 
 
-        #POST
-        $group->post('/user', [UserController::class, 'postUser']);
-        $group->post('/reminder', [ReminderController::class, 'postReminder']);
-        $group->post('/family-group', [FamilyGroupController::class, 'postFamilyGroup']);
-        $group->post('/pet', [PetController::class, 'postPet']);
-        $group->post('/medical-ctrl', [MedicalCtrlController::class, 'postMedicalCtrl']);
-        $group->post('/treatment', [TreatmentController::class, 'postTreatment']);    
-    /**
-     * Faltan los controles y servicios de tratamientos...
-     */
+    // #POST
+    // $group->post('/user', [UserController::class, 'postUser']);
+    // $group->post('/reminder', [ReminderController::class, 'postReminder']);
+    // $group->post('/family-group', [FamilyGroupController::class, 'postFamilyGroup']);
+    // $group->post('/pet', [PetController::class, 'postPet']);
+    // $group->post('/medical-ctrl', [MedicalCtrlController::class, 'postMedicalCtrl']);
+    // $group->post('/treatment', [TreatmentController::class, 'postTreatment']);    
+
     $group->group('/secure', function (RouteCollectorProxy $auth) {
         // $group->get('/user', [UserController::class, 'getUserByEmail']);
 
